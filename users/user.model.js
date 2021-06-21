90c2007667b610bd241b96d906e5b37409dc79d5
kunwalle <khang.luong@nfq.asia>
2021-06-22 00:20:57 +0700

    'init'

---
+++ users/user.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    username: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now },
+    notification: [{
+        price: {type: Number},
+        oldPrice: {type: Number},
+    }],
+    currency: {type: String, default: 'USDT_BTC'},
+    timer: {type: Number, default: 30},
+});
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+        delete ret._id;
+        delete ret.hash;
+    }
+});
+
+module.exports = mongoose.model('User', schema);
